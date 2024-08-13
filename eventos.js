25f84b5b9c2b7a58f627aaa47936808c5e76e615
camilacanzobre24 <camilacanzobre@hotmail.com>
2024-08-13 19:29:56 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     var div = document.querySelector('div');
+    var boton = document.querySelector('button');
     div.addEventListener("click", function() {
         alert('¡Hola! Soy el div');
+    });
+    boton.addEventListener("click", function(event) {
+        event.stopPropagation(); 
     });
 });
